01b3e585d652c707600e7742a27a0636ef6c06b1
Ganesh-Kumatole <workspace.ganeshkumatole@gmail.com>
2025-11-04 18:13:00 +0530

    Patterns (pt. 2)

--- 04_Patterns/script.js
+++ 04_Patterns/script.js
@@ -101,7 +101,83 @@ class Patterns {
       console.log(row);
     }
   }
+
+  pattern07(n) {
+    /*
+     *
+     * *
+     * * *
+     * * * *
+     * * *
+     * *
+     *
+     */
+
+    // Until the mid part...
+    for (let i = 0; i < n; i++) {
+      let row = '';
+      // Append stars...
+      for (let j = 0; j <= i; j++) {
+        row += '*';
+      }
+      // Append white spaces...
+      for (let k = 0; k <= n - i - 1; k++) {
+        row += '';
+      }
+      console.log(row);
+    }
+
+    // After mid part...
+    for (let i = 1; i <= n - 1; i++) {
+      let row = '';
+
+      // Append stars...
+      for (let j = 0; j < n - i; j++) {
+        row += '*';
+      }
+
+      // Append white spaces...
+      for (let k = 0; k < i; k++) {
+        row += '';
+      }
+      console.log(row);
+    }
+  }
+
+  pattern08(n) {
+    /*
+      1
+      0 1
+      1 0 1
+      0 1 0 1
+      1 0 1 0 1
+    */
+
+    for (let i = 0; i < n; i++) {
+      let row = ' ';
+      let startDigit = i % 2 === 0 ? 1 : 0;
+      let prevDigit;
+
+      // Append digits
+      for (let j = 0; j <= i; j++) {
+        if (j === 0) {
+          row += `${+startDigit} `;
+          prevDigit = startDigit;
+        } else {
+          row += `${+!prevDigit} `;
+          prevDigit = +!prevDigit;
+        }
+      }
+
+      // Append white spaces
+      for (let k = 0; k < n - i - 1; k++) {
+        row += ' ';
+      }
+
+      console.log(row);
+    }
+  }
 }
 
 const ptnrObj = new Patterns();
-ptnrObj.pattern06(10);
+ptnrObj.pattern08(8);
